081cd8bee3084860daa42c248e20afa3ebcfffa5
Rajvardhan Singh Ranawat <146507318+Rajvardhan222@users.noreply.github.com>
2025-09-01 14:59:27 +0000

    Update Twitter username in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Rajvardhan Ranawat is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://rajvardhan.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@rajvardhanranawat',
+    twitterUsername: '@Rajvardhan_r',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
